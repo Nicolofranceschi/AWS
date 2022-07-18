c74c2123fc6f5f34c008f8a5ed98627e1a8cbdb4
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2022-07-18 17:26:17 +0200

    Initial commit

--- bin/cdk-vpc.ts
+++ bin/cdk-vpc.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import { CdkVpcStack } from '../lib/cdk-vpc-stack';
 
 const app = new App();
-new CdkDemoStack(app, 'CdkDemoStack', {
+new CdkVpcStack(app, 'CdkVpcStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/cdk-vpc-stack.ts
+++ lib/cdk-vpc-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps, aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class CdkDemoStack extends Stack {
+export class CdkVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
